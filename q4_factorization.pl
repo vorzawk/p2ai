46f08129c250c3d8b6157c776f89e5e2775b6d48
vorzawk <vrabhilash92@gmail.com>
2017-12-05 21:14:18 -0600

    q4 done

--- q4_factorization.pl
+++ q4_factorization.pl
@@ -1,7 +1,17 @@
 #Rules
 divisible(N,X) :- M is N mod X, M=0. 
-% Factors without considering 1 and the number itself
-factors(N,F) :- M is N-1, between(2,M,F), divisible(N,F).
-primeFactors(N,P) :- factors(N,P), \+ factors(P,_).
+%We want to return the list of prime factors in P, however we cannot build the list using P since variables in prolog can be bound only once.
+%Instead, recursively accumulate the factors and then bind the list to P
+primeFactors(N,P) :- 
+    listPrimeFactors(N,2,[],P).
 
-#Outputs all the prime factors but can't seem to figure out how to put them in a list!
+%If For each number between 2 and N, check if it is a prime factor and add it to the list if it is. Otherwise, just proceed to the next number.
+listPrimeFactors(N,K,Acc,ListPrime) :- K=<N, primeFactorCheck(N,K), Knext is K+1, listPrimeFactors(N,Knext,[K|Acc],ListPrime).
+listPrimeFactors(N,K,Acc,ListPrime) :- K=<N, \+ primeFactorCheck(N,K), Knext is K+1, listPrimeFactors(N,Knext,Acc,ListPrime).
+
+%Base condition for recursion
+listPrimeFactors(N,M,Acc,Acc) :- N=M.
+
+primeFactorCheck(N,P) :- M is N-1, between(2,M,P), divisible(N,P), \+ primeFactorCheck(P,_).
+
+
